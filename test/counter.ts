58758a190d816aebd4c2094112db8c105c69fdb3
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-05-12 18:04:09 +0700

    chore: prettier

--- test/counter.ts
+++ test/counter.ts
@@ -1,12 +1,12 @@
 export function createCounter() {
-  const button = document.createElement('button');
-  button.textContent = 'Count: 0';
+  const button = document.createElement("button");
+  button.textContent = "Count: 0";
   let count = 0;
 
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
     count++;
     button.textContent = `Count: ${count}`;
- });
+  });
 
   return button;
 }
