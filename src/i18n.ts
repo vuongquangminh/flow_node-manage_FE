334e03043aef00eba90bc9b49e5cfd22f4c7940e
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-05-27 17:14:07 +0700

    chore

--- src/i18n.ts
+++ src/i18n.ts
@@ -23,10 +23,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "vi", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
+    lng: "vi", // language to use
     interpolation: {
       escapeValue: false // react already safes from xss
     }
