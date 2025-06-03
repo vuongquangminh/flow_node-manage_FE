9048b8b03d1aef19deeb6c6caac7921b30398174
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-06-03 15:09:38 +0700

    todo: cancel friend chat

--- src/components/Layout/LayoutPage.tsx
+++ src/components/Layout/LayoutPage.tsx
@@ -13,17 +13,17 @@ const LayoutPage = () => {
   const account = useGetUserQuery();
   const socket = useContext(SocketContext);
   const [keyRender, setKeyRender] = useState(0);
-  const user = getLocalStorage({ key: "user" });
-  if (!user) {
-    navigate("/");
-  }
+  // const user = getLocalStorage({ key: "user" });
+  // if (!user) {
+  //   navigate("/");
+  // }
 
-  const options = account?.data
-    ?.filter((item) => item._id !== user._id)
-    ?.map((item) => ({
-      value: item._id,
-      label: `${item.name} - ${item.email}`,
-    }));
+  // const options = account?.data
+  //   ?.filter((item) => item._id !== user._id)
+  //   ?.map((item) => ({
+  //     value: item._id,
+  //     label: `${item.name} - ${item.email}`,
+  //   }));
 
   const handleChange = (value: number) => {
     socket.emit("add-friend", {
@@ -39,7 +39,7 @@ const LayoutPage = () => {
     <SocketContext.Provider value={socket}>
       <Layout style={{ height: "100vh" }}>
         <Header className="flex justify-center items-center bg-cyan-700">
-          <Select
+          {/* <Select
             className="w-full"
             showSearch
             allowClear
@@ -47,7 +47,7 @@ const LayoutPage = () => {
             optionFilterProp="label"
             onChange={handleChange}
             options={options}
-          />
+          /> */}
           <Link to={"/chatbot"} className="w-10 ml-3">
             <img className="h-full w-full rounded" src="/logo-gpt.jpg" alt="" />
           </Link>
