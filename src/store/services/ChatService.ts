b64a268cedf8ca913146ece9cbad727b75693c0c
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-06-04 17:37:36 +0700

    cls

--- src/store/services/ChatService.ts
+++ src/store/services/ChatService.ts
@@ -7,8 +7,7 @@ export const chatApi = createApi({
     baseUrl: import.meta.env.VITE_API_URL,
     prepareHeaders: (headers) => {
       const token = getLocalStorage({ key: "token" });
-
-      // If we have a token set in state, let's assume that we should be passing it.
+      
       if (token) {
         headers.set("authorization", `Bearer ${token}`);
       }
--- src/store/services/FriendService.ts
+++ src/store/services/FriendService.ts
@@ -8,7 +8,6 @@ export const friendApi = createApi({
     prepareHeaders: (headers) => {
       const token = getLocalStorage({ key: "token" });
 
-      // If we have a token set in state, let's assume that we should be passing it.
       if (token) {
         headers.set("authorization", `Bearer ${token}`);
       }
