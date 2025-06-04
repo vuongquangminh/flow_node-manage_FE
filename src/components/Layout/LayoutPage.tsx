38b1b20d3544a9b2fa86fd6affa439ccd54da514
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-06-04 17:33:00 +0700

    update: delete sidebar add friend

--- src/components/Layout/LayoutPage.tsx
+++ src/components/Layout/LayoutPage.tsx
@@ -1,35 +1,15 @@
-import { Col, Layout, Row, Select } from "antd";
+import { Col, Layout, Row } from "antd";
 import SideBar from "./Sidebar";
-import { Link, Outlet, useNavigate } from "react-router-dom";
-import { getLocalStorage } from "../../hooks/localStorage";
-import { useGetUserQuery } from "../../store/services/UserService";
+import { Link, Outlet} from "react-router-dom";
 import { useContext, useState } from "react";
 import { SocketContext } from "../../utils/SocketContext";
 
 const { Header } = Layout;
 
 const LayoutPage = () => {
-  const navigate = useNavigate();
-  const account = useGetUserQuery();
   const socket = useContext(SocketContext);
   const [keyRender, setKeyRender] = useState(0);
-  // const user = getLocalStorage({ key: "user" });
-  // if (!user) {
-  //   navigate("/");
-  // }
 
-  // const options = account?.data
-  //   ?.filter((item) => item._id !== user._id)
-  //   ?.map((item) => ({
-  //     value: item._id,
-  //     label: `${item.name} - ${item.email}`,
-  //   }));
-
-  const handleChange = (value: number) => {
-    socket.emit("add-friend", {
-      id: value,
-    });
-  };
   socket.on("update-friend", (data) => {
     console.log(data);
     setKeyRender((pre) => pre + 1);
@@ -39,15 +19,6 @@ const LayoutPage = () => {
     <SocketContext.Provider value={socket}>
       <Layout style={{ height: "100vh" }}>
         <Header className="flex justify-center items-center bg-cyan-700">
-          {/* <Select
-            className="w-full"
-            showSearch
-            allowClear
-            placeholder="Select a person"
-            optionFilterProp="label"
-            onChange={handleChange}
-            options={options}
-          /> */}
           <Link to={"/chatbot"} className="w-10 ml-3">
             <img className="h-full w-full rounded" src="/logo-gpt.jpg" alt="" />
           </Link>
