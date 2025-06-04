8582b06476e3c7a41e8d5e8e1fe857c2559911f9
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-06-04 17:53:17 +0700

    refactor: headerRequest of baseQuery

--- src/store/services/ChatService.ts
+++ src/store/services/ChatService.ts
@@ -1,19 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { getLocalStorage } from "../../hooks/localStorage";
+import headerRequest from "../../utils/headerRequest";
 
 export const chatApi = createApi({
   reducerPath: "chatApi",
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_URL,
-    prepareHeaders: (headers) => {
-      const token = getLocalStorage({ key: "token" });
-      
-      if (token) {
-        headers.set("authorization", `Bearer ${token}`);
-      }
-
-      return headers;
-    },
+    prepareHeaders: (headers) => headerRequest(headers),
   }),
   tagTypes: ["Chat"],
   endpoints: (build) => ({
---
+++ src/utils/headerRequest.ts
@@ -0,0 +1,11 @@
+const headerRequest = (headers: Headers) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    headers.set("authorization", `Bearer ${token}`);
+  }
+
+  return headers;
+};
+
+export default headerRequest;
