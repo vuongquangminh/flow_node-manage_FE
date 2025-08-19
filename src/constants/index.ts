e160ae22aa5d97c2db9f28e53d468689b3a5ade5
vuongquangminh120802@gmail.com <vuongquangminh120802@gmail.com>
2025-08-19 23:23:05 +0700

    chore

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,30 +1,30 @@
 export const slick_settings = {
   focusOnSelect: true,
   // infinite: true,
-  slidesToShow: 5,
+  slidesToShow: 1,
   slidesToScroll: 1,
   speed: 500,
   // autoplay: true,
-  // responsive: [
-  //   {
-  //     breakpoint: 1024, // dưới 1024px
-  //     settings: {
-  //       slidesToShow: 3,
-  //     },
-  //   },
-  //   {
-  //     breakpoint: 768, // dưới 768px
-  //     settings: {
-  //       slidesToShow: 2,
-  //     },
-  //   },
-  //   {
-  //     breakpoint: 600, // dưới 600px
-  //     settings: {
-  //       slidesToShow: 1,
-  //     },
-  //   },
-  // ],
+  responsive: [
+    {
+      breakpoint: 1024, // dưới 1024px
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 768, // dưới 768px
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 600, // dưới 600px
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
 };
 
 export const slick_settings_banner = {
