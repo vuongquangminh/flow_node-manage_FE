8a4adc18a659371a85842bf10b9738b382ddd3c6
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-08-20 09:06:40 +0700

    fix socket

--- src/utils/SocketContext.ts
+++ src/utils/SocketContext.ts
@@ -1,13 +1,9 @@
 // SocketContext.js
 import { createContext } from "react";
 import { io } from "socket.io-client";
-import { getLocalStorage } from "../hooks/localStorage";
 
 export const createSocket = () => {
-  const user = getLocalStorage({ key: "user" });
-  const socket = io(import.meta.env.VITE_API_URL, {
-    query: { user: JSON.stringify(user) },
-  });
+  const socket = io(import.meta.env.VITE_API_URL);
   return socket;
-} 
+};
 export const SocketContext = createContext(createSocket);
